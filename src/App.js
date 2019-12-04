17cb083f45e59f0ed1a7f6bb9846a2cb750ea491
joharbatta <joharbatta@gmail.com>
2019-12-05 01:43:17 +0530

    Dynamic Content: Added 'Delete' Button

--- src/App.js
+++ src/App.js
@@ -34,6 +34,9 @@ class App extends Component {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                <button className="custom-btn delete-btn">Delete</button>
+                </span>
               </div>
             })
           }
