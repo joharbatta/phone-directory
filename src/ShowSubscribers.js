7837865ef97785d1301bec18b55ecd1ba311660b
joharbatta <joharbatta@gmail.com>
2019-12-08 02:38:22 +0530

    Added Delete Subscriber

--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -4,7 +4,9 @@ import './ShowSubscribers.css';
 import { Link } from 'react-router-dom';
 
 class ShowSubscribers extends Component {
-  
+  onDeletedClick = (subscriberId) => {
+    this.props.deleteSubscriberHandler(subscriberId); 
+  }
   render() {
 
     return (
@@ -25,7 +27,7 @@ class ShowSubscribers extends Component {
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
                 <span className="grid-item action-btn-container">
-                <button className="custom-btn delete-btn">Delete</button>
+                <button className="custom-btn delete-btn" onClick={this.onDeletedClick.bind(this, sub.id)}>Delete</button>
                 </span>
               </div>
             })
